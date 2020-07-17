fce2a6fde8d2ccd245ee75bffb28f64f2c8994de
Jungho Sung <jungho.sung@Macan.local>
2020-07-17 14:41:38 +0900

    Updates

---
+++ precache-manifest.7c7795abf3fb325a76dbdd82813cba53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33558d9d8ce8c15d4da02eb13e45ead3",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "67ed543e55a959bfb9de",
+    "url": "/movie_app_2020/static/css/main.007d5fc9.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ed543e55a959bfb9de",
+    "url": "/movie_app_2020/static/js/main.ae2db320.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
